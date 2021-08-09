e9a5e83e87bc5f479f2744db5c30e3b635948ee3
Iván Amat <dev@ivanamat.es>
2021-08-09 12:24:30 +0200

    action.yaml Workflow and main.ts updated

--- src/main.ts
+++ src/main.ts
@@ -16,25 +16,11 @@ async function run(): Promise<void> {
     const [owner, repo] = inputs.repository.split('/')
 
     const octokit = github.getOctokit(inputs.token)
-    
-    // List workflows via API, and handle paginated results
-    const workflows: ActionsGetWorkflowResponseData[] =
-      await octokit.paginate(octokit.actions.listRepoWorkflows.endpoint.merge({ owner, repo, ref, inputs }))
-
-    // Debug response if ACTIONS_STEP_DEBUG is enabled
-    core.debug('### START List Workflows response data')
-    core.debug(JSON.stringify(workflows, null, 3))
-    core.debug('### END:  List Workflows response data')
-
-    // Locate workflow either by name or id
-    const workflow = workflows.find((workflow) => workflow.name === inputs.workflow_id || workflow.id.toString() === inputs.workflow_id)
-    if(!workflow) throw new Error(`Unable to find workflow '${inputs.workflow_id}' in ${owner}/${repo}`)
-    console.log(`Workflow id is: ${workflow.id}`)
 
     await octokit.rest.actions.createWorkflowDispatch({
         owner: owner,
         repo: repo,
-        workflow_id: workflow.id,
+        workflow_id: inputs.workflow_id,
         ref: inputs.ref,
         inputs: JSON.parse(inputs.workflow_inputs)
     })
