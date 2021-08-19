8bcfc75371f0f269c18a1954764cf36396346e7a
Iván Amat <iamat@atsistemas.com>
2021-08-19 14:00:24 +0200

    Fix Auth

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ async function run(): Promise<void> {
     ) {
       core.setFailed(
         '[Error]: Authorization is required!. Yoy need to provide a Personal Access Token or Application Credentials. Application Credentials require: appId, privateKey, clientId, clientSecret and installationId'
-      )
+      );
       
       exit 1;
     }
