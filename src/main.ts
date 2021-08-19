f19df6b5255d6e302811d1e64048fa5201ebd7ef
Iván Amat <iamat@atsistemas.com>
2021-08-19 13:31:11 +0200

    Check Auth

--- src/main.ts
+++ src/main.ts
@@ -22,12 +22,12 @@ async function run(): Promise<void> {
 
     const [owner, repo] = inputs.repository.split('/')
     
-    if(inputs.token == "" && 
-        (inputs.appId == "" || 
-        inputs.privateKey == "" || 
-        inputs.clientId == "" || 
-        inputs.clientSecret == "" || 
-        inputs.installationId == "")) {
+    if(inputs.token === "" && 
+        (inputs.appId === "" || 
+        inputs.privateKey === "" || 
+        inputs.clientId === "" || 
+        inputs.clientSecret === "" || 
+        inputs.installationId === "")) {
       core.setFailed(
         '[Error]: Authorization is required!. Yoy need to <strong>provide a Personal Access Token or Application Credentials</strong>.\nA pplication Credentials require: appId, privateKey, clientId, clientSecret and installationId'
       )
