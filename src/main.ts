3d8ba452b5d53a57437c337d478b11a4dab15fc9
Iván Amat <iamat@atsistemas.com>
2021-08-20 11:42:07 +0200

    APP Installations

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import { Octokit } from '@octokit/rest'
 import {createAppAuth} from '@octokit/auth-app'
 import {inspect} from 'util'
 
